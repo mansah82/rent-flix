0b7a920b581047f3aff0088cfaf89fecf26f99d0
Mattias170624 <ExozTic_170624@outlook.com>
2022-05-02 18:02:30 +0200

    Added hover effect and general styling to movieCards

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -23,7 +23,7 @@ const HomePage = ({ setMovie }) => {
             popularContent = movie.map((data) => (
                 <li className='movieCard' key={data.id}>
                     <Link to={"/info"} onClick={() => setMovie(data)}>
-                        <img className='movieImage' src={picturePath + data.poster_path} />
+                        <img className='movieImage' src={picturePath + data.poster_path} alt='Movie poster' />
                         <h3>{data.title}</h3>
                     </Link>
                 </li>
@@ -35,17 +35,12 @@ const HomePage = ({ setMovie }) => {
             break
     }
 
-
-
     useEffect(() => {
         fetchPopularMovies(dispatch);
     }, [])
 
-
     return (
         <div id='homePage'>
-
-
             <h1 id='categoryTitle'>Popular</h1>
             <section id='movieContainer'>
                 {popularContent}
@@ -55,9 +50,7 @@ const HomePage = ({ setMovie }) => {
 }
 
 async function fetchPopularMovies(dispatch) {
-
     dispatch(actions.isFetching());
-
     const url = 'https://api.themoviedb.org/3/movie/popular?api_key=ace7b669ec91ad7702878aa98fd99d60&language=en-US&page=1'
 
     try {
@@ -72,31 +65,3 @@ async function fetchPopularMovies(dispatch) {
 };
 
 export default HomePage;
-
-
-   // if (status === STATUS.NORMAL) {
-
-    //     popularContent = 'Ready for movies'
-    // } else if (status === STATUS.FETCHING) {
-
-    //     popularContent = 'fetching movies'
-    // } else if (status === STATUS.SUCCESS) {
-
-    //     popularContent = movie.map((mov) => (
-
-    //         <div key={mov.id} className="column" onClick={() => setMovie(mov)}>
-    //             <div className='row'>
-    //                 <img className='movie-poster' src={picturePath + mov.poster_path} />
-    //                 <p className='movie-title'>{mov.title}</p>
-    //             </div>
-    //         </div>
-    //     ))
-    // }
-
- // return (
-    //     <Link to={"/info"}>
-    //         <div className="row">
-    //             {popularContent}
-    //         </div>
-    //     </Link>
-    // );
