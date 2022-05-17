3e667c6529ecefe896a4b15cb543c11c386457c7
Mattias170624 <ExozTic_170624@outlook.com>
2022-05-17 23:04:01 +0200

    Added snapshot on movie comments.

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,16 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './css/commentSection.css';
 import { FaRegComments } from 'react-icons/fa'
 import db from '../firebase/firebase';
-import { addDoc, onSnapshot, updateDoc } from 'firebase/firestore';
-import { doc, setDoc, arrayUnion } from "firebase/firestore";
+import { doc, setDoc, arrayUnion, onSnapshot, addDoc, updateDoc, collection } from "firebase/firestore";
 
 
 const CommentSection = ({ selectedMovie }) => {
+    const mounted = useRef(false);
 
     useEffect(() => {
-        appendFirestoreData(selectedMovie)
+        mounted.current = true;
 
+        const unsubscribe = onSnapshot(doc(db, "movies", selectedMovie.original_title), (doc) => {
+            if (doc.data() == undefined) return
+
+            addPreviousComments(doc.data())
+        });
+
+        // Triggers when component is unmounted
+        return () => {
+            mounted.current = false;
+            unsubscribe()
+        };
     }, [])
 
     return (
@@ -36,14 +47,27 @@ const CommentSection = ({ selectedMovie }) => {
     async function createNewComment(selectedMovie) {
         const textInput = document.getElementById('textInputField');
 
-        const movieRef = doc(db, "movies", selectedMovie.title);
-        setDoc(movieRef, { comment: arrayUnion(...[textInput.value]) }, { merge: true });
+        if (textInput.value == '') {
+            textInput.placeholder = "Field can't be empty!"
+        } else {
+            textInput.placeholder = "Your thoughts..."
 
+            const movieRef = doc(db, "movies", selectedMovie.title);
+            setDoc(movieRef, { comment: arrayUnion(...[textInput.value]) }, { merge: true });
+            textInput.value = ''
+        }
     }
 
-    function appendFirestoreData(selectedMovie) {
-        console.log('Movie id to fetch chat from:', selectedMovie.id);
-        // Todo
+    function addPreviousComments(oldList) {
+        const currentList = document.getElementById("commentList");
+        currentList.innerHTML = '';
+
+        for (let i = 0; i < oldList.comment.length; i++) {
+            const newComment = document.createElement("li");
+            newComment.innerHTML = oldList.comment[i];
+
+            currentList.prepend(newComment)
+        }
     }
 }
 
